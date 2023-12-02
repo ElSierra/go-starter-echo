e305a11b2d2376c47509b6aaba3b5c7a9358088f
Isaac Ojo <hojoisaac@gmail.com>
2023-12-02 14:08:38 +0100

    init

--- main.go
+++ main.go
@@ -1,16 +1,22 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/elsierra/go-echo-zik/routes"
+	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
-	// Echo instance
+	godotenv.Load()
 	e := echo.New()
 
-	// Middleware
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"*"},
+		AllowMethods: []string{http.MethodGet, http.MethodHead, http.MethodPut, http.MethodPatch, http.MethodPost, http.MethodDelete},
+	}))
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 
